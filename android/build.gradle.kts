f06cac949ddc77a157257d59c4a77ded95a636d7
Lalo <gonzalo.alvalon@gmail.com>
2026-09-22 20:03:02 +0200

    chore: bump the version to 0.4.0
    Add the 0.4.0 release notes for the Play upload.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "dev.lalogo.passtick"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
         testInstrumentationRunner = "org.ligi.passandroid.AppReplacingRunner"
         vectorDrawables.useSupportLibrary = true
     }
